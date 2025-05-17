f9aaf3244a9f3a080b5ff1dabcfab8991ead8bf5
Annelise <annelise.celma@gmail.com>
2025-05-17 15:21:15 -0400

    premieres pages

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+// import {NavigationContainer} from '@reaact-navigation/native';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+
+// const App=()=>{
+//     return(
+//         <NavigationContainer>
+
+//         </NavigationContainer>
+//     );
+// };
+
 registerRootComponent(App);
